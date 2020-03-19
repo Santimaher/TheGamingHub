8c57dff908b400199a966074f4e92d074a885e5a
Santimaher <48292512+Santimaher@users.noreply.github.com>
2020-03-19 17:56:50 +0100

    Esqueleto 2.0

---
+++ src/main/java/org/tfgdp2/com/TheGamingHubApplication.java
@@ -0,0 +1,13 @@
+package org.tfgdp2.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGamingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGamingHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tfgdp2/com/TheGamingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tfgdp2.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGamingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
