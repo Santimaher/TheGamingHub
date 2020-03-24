271c9ccab69e07efce33827be8ed46223db51c7a
IvanPanber <62348504+IvanPanber@users.noreply.github.com>
2020-03-24 12:29:22 +0100

    Registro y RUD Usuario

--- src/main/java/org/tfgdp2/com/helper/H.java
+++ src/main/java/org/tfgdp2/com/helper/H.java
@@ -12,24 +12,24 @@ public class H {
 	 * @param s   la sesión activa
 	 * @throws DangerException si el rol no coincide con el del usuario activo
 	 */
-	public static void isRolOK(String rol, HttpSession s) throws DangerException {
-		Persona persona = null;
-
-		if (s.getAttribute("persona") != null) {
-			persona = (Persona) s.getAttribute("persona");
-		}
-
-		// Ya sé quién ha hecho login, y si alguien lo ha hecho
-		
-		if (persona == null) { // anon
-			if (rol != "anon") {
-				PRG.error("Rol inadecuado");
-			}
-		} else { // Auth o admin
-			if (!persona.getLoginname().equals("admin") && rol.equals("admin")) { // anon
-				PRG.error("Rol inadecuado");
-			}
-		}
-
-	}
+//	public static void isRolOK(String rol, HttpSession s) throws DangerException {
+//		Persona persona = null;
+//
+//		if (s.getAttribute("persona") != null) {
+//			persona = (Persona) s.getAttribute("persona");
+//		}
+//
+//		// Ya sé quién ha hecho login, y si alguien lo ha hecho
+//		
+//		if (persona == null) { // anon
+//			if (rol != "anon") {
+//				PRG.error("Rol inadecuado");
+//			}
+//		} else { // Auth o admin
+//			if (!persona.getLoginname().equals("admin") && rol.equals("admin")) { // anon
+//				PRG.error("Rol inadecuado");
+//			}
+//		}
+//
+//	}
 }
