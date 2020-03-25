69d1b7aa8ce71d0fe6353e03e2b20bc25e894472
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-03-25 12:14:24 +0100

    Nominacion Controller y Repositorio

---
+++ src/main/java/org/tfgdp2/com/repository/NominacionRepository.java
@@ -0,0 +1,8 @@
+package org.tfgdp2.com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tfgdp2.com.domain.Nominacion;
+
+public interface NominacionRepository extends JpaRepository<Nominacion, Long>{
+
+}
