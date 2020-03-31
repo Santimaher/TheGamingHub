5793274c42e2cdadd36b7068567571d474128d79
Santimaher <48292512+Santimaher@users.noreply.github.com>
2020-03-31 16:20:10 +0200

    Correcion falta de anotaciones

--- src/main/java/org/tfgdp2/com/repository/ParticipanteRepository.java
+++ src/main/java/org/tfgdp2/com/repository/ParticipanteRepository.java
@@ -1,8 +1,10 @@
 package org.tfgdp2.com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tfgdp2.com.domain.Participante;
 
+@Repository
 public interface ParticipanteRepository extends JpaRepository<Participante,Long>{
 
 }
--- src/main/java/org/tfgdp2/com/repository/UsuarioRepository.java
+++ src/main/java/org/tfgdp2/com/repository/UsuarioRepository.java
@@ -2,8 +2,10 @@ package org.tfgdp2.com.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tfgdp2.com.domain.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 	public Usuario getByLoginnameOrEmail(String lognom,String email);
 }
