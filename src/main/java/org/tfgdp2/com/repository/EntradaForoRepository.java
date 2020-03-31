5575f6cdafa48734611d409584f68df6dbd66fe2
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-03-31 13:34:02 +0200

    .

--- src/main/java/org/tfgdp2/com/repository/EntradaForoRepository.java
+++ src/main/java/org/tfgdp2/com/repository/EntradaForoRepository.java
@@ -1,8 +1,10 @@
 package org.tfgdp2.com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tfgdp2.com.domain.EntradaForo;
 
+@Repository
 public interface EntradaForoRepository extends JpaRepository<EntradaForo, Long>{
 
 }
