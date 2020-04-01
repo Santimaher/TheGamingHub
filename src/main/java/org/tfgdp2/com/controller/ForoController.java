1407cc038dad48e031a835c45f0e7846a5a2bf9a
Santimaher <48292512+Santimaher@users.noreply.github.com>
2020-04-01 12:21:17 +0200

    Enlace de Juego con Foro

--- src/main/java/org/tfgdp2/com/controller/ForoController.java
+++ src/main/java/org/tfgdp2/com/controller/ForoController.java
@@ -4,8 +4,7 @@ package org.tfgdp2.com.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +27,9 @@ public class ForoController {
 	private JuegoRepository repoJuego;
 	
 	@GetMapping("r")
-	public String read(ModelMap m) {
-		m.put("entradas", repoEntrada.findAll());
+	public String read(ModelMap m,@RequestParam("id") Long id) {
+		m.put("juego", repoJuego.getOne(id));
+		m.put("foros", repoForo.findAllByJuego_id(id));
 		m.put("view", "foro/r");
 		return "/_t/frame";
 	}
--- src/main/java/org/tfgdp2/com/repository/ForoRepository.java
+++ src/main/java/org/tfgdp2/com/repository/ForoRepository.java
@@ -1,5 +1,7 @@
 package org.tfgdp2.com.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,6 @@ import org.tfgdp2.com.domain.Foro;
 @Repository
 public interface ForoRepository extends JpaRepository<Foro, Long> {
 
+	public List<Foro> findAllByJuego_id(Long idJuego);
+	
 }
