efedcd9348bce15d735030f6e4b597c4365c99e0
Santimaher <48292512+Santimaher@users.noreply.github.com>
2020-05-11 12:06:46 +0200

    Metodo de votar

--- src/main/java/org/tfgdp2/com/repository/PremioJuegoRepository.java
+++ src/main/java/org/tfgdp2/com/repository/PremioJuegoRepository.java
@@ -1,10 +1,12 @@
 package org.tfgdp2.com.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.tfgdp2.com.domain.Foro;
 import org.tfgdp2.com.domain.Premio_Juego;
 
 @Repository
 public interface PremioJuegoRepository extends JpaRepository<Premio_Juego, Long> {
-
 }
