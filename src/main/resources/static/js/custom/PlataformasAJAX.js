32f3a8b55a0ad2290149060dd396f759cddad9d4
santi <santi@PORTATILSANTI>
2020-05-15 13:46:25 +0200

    Parte 2 terminada

--- src/main/resources/static/js/custom/PlataformasAJAX.js
+++ src/main/resources/static/js/custom/PlataformasAJAX.js
@@ -2,15 +2,17 @@ function busca() {
 	clear();
 	var aguja = document.getElementById('busca').value;
 	var res = getJSON();
-	var arr = res[0].plataformas
+	var arr = res[0].plataformas;
 	for (let consola of arr) {
 		if(aguja == consola.familia){
-			//document.getElementById('qwe').innerHTML += '<button onclick="alert(this.value+"/"+this.id)" id="'+consola.id+'">' + consola.nombre+ "</button>";
+			// document.getElementById('qwe').innerHTML += '<button
+			// onclick="alert(this.value+"/"+this.id)" id="'+consola.id+'">' +
+			// consola.nombre+ "</button>";
 			var boton = document.createElement("button");
 			boton.id = consola.id;
 			var texto=document.createTextNode(consola.nombre);
 			boton.append(texto);
-			boton.setAttribute("onclick","anadir(this.value,this.id");
+			boton.setAttribute("onclick",'anadir("'+consola.nombre+'",this.id)');
 			document.getElementById('qwe').append(boton);
 			}		
 	}
@@ -40,19 +42,23 @@ function getJSON() {
 }
 
 function anadir(nombre,id){
-	//document.getElementById('123').innerHTML = '<div class="chip" name="idPlataforma[]" value="'+id+'">'nombre'<i class="close material-icons">close</i></div>'
-	var divo = document.createElement("div");
-	divo.class="chip";
+	// document.getElementById('123').innerHTML = '<div class="chip"
+	// name="idPlataforma[]" value="'+id+'">'nombre'<i class="close
+	// material-icons">close</i></div>'
+	var divo = document.createElement("div");	
+	divo.classList.add("chip");
 	var texto2 = document.createTextNode(nombre);
 	divo.append(texto2);
 	var oculto = document.createElement("input");
 	oculto.type="hidden";
 	oculto.name="idPlataforma[]";
-	oculto.value="id";
+	oculto.value=id;
 	divo.append(oculto);
 	var cerrar = document.createElement("i");
-	cerrar.class="close material-icons";
-	cerrar.value="close";
+	cerrar.classList.add("close");
+	cerrar.classList.add("material-icons");	 
+	var texto3 = document.createTextNode("close");
+	cerrar.append(texto3);
 	divo.append(cerrar);
 	document.getElementById('123').append(divo);
 }
