bb71c0a08beeab8c6bd127b8713bdff9dceadbaf
santi <santi@PORTATILSANTI>
2020-05-15 12:44:24 +0200

    Pseudo funciona

--- src/main/resources/static/js/custom/PlataformasAJAX.js
+++ src/main/resources/static/js/custom/PlataformasAJAX.js
@@ -1,23 +1,33 @@
+function busca() {
+	document.getElementById('qwe').innerHTMl = " ";
+	var aguja = document.getElementById('busca').value;
+	var res = getJSON();
+	var arr = res[0].plataformas
+	for (let consola of arr) {
+		if(aguja == consola.familia){
+			document.getElementById('qwe').innerHTML += '<button value="'+consola.id+'">' + consola.nombre
+			+ "</button>";}
+		
+	}
+	}
+		
+	
+
+
 
 function getJSON() {
-	  var resp = [];
-	 $.ajax({
-	  url: '/js/custom/plataformas.txt',
-	  type: 'GET',
-	  dataType: 'json',
-	  async: false,
-	  success : function(data) {
-	    console.log(data);
-	    resp.push(data);
-	    for ( var consola in data.plataformas) {
-			$("#expositor").append("<button>"+consola.nombre+"</button>");
+	var resp = [];
+	$.ajax({
+		url : '/js/custom/plataformas.txt',
+		type : 'GET',
+		dataType : 'json',
+		async : false,
+		success : function(data) {
+			resp.push(data);
+		},
+		error : function(req, err) {
+			console.log(err);
 		}
-	  }, error : function(req, err) {
-	      console.log(err);
-	  }
 	})
-	console.log(resp);
 	return resp;
-	}
-
-getJSON();
+}
