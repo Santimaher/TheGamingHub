4e49191e8a81aac8d4760f472eb21b6bad06588a
santi <santi@PORTATILSANTI>
2020-05-25 15:56:00 +0200

    urls

--- src/main/java/org/tfgdp2/com/controller/MainController.java
+++ src/main/java/org/tfgdp2/com/controller/MainController.java
@@ -65,7 +65,7 @@ public class MainController {
 	}
 	@GetMapping("/danger")
 	public String danger(ModelMap m) {
-		m.put("view", "/anonymous/danger.html");
+		m.put("view", "anonymous/danger.html");
 		m.put("script", "PlataformasAJAX");
 		return "_t/frame";
 	}
