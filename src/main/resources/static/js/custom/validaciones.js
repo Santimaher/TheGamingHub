0ad755924284fd8f7d2be9d5f899d81843dd354c
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-05-26 18:24:02 +0200

    puede ser este el fin de las validaciones?

--- src/main/resources/static/js/custom/validaciones.js
+++ src/main/resources/static/js/custom/validaciones.js
@@ -1,5 +1,5 @@
 function validarN_Partic() {
-	 var valido;
+	 var valido=false;
 	 var n=document.partForm.nombre.value;
 	 var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
 	 var t=patt.test(n);
@@ -9,14 +9,13 @@ function validarN_Partic() {
 		 
 	 M.toast({html: 'El formato del nombre no es correcto.'}); 
 	 document.partForm.nombre.style.color='red';
-	 valido=false;
 	 
 	 }
 	 return valido;
 	 }
 
 	 function validarA_Partic(){
-	 var valido;
+	 var valido=false;
 	 var pa=/^[A-Z]{1}[a-zA-ZñÑçÇáéíóú ]{2,40}$/;
 	 var a=document.partForm.apellido.value;
 	 var t=pa.test(a);
@@ -25,20 +24,18 @@ function validarN_Partic() {
 	 }else{
 		M.toast({html: 'El formato de los apellidos no es correcto.'});
 	 document.partForm.apellido.style.color='red';
-	 valido=false;
 	 }
 
 	 return valido;
 	 }
 
 	 function validarC_Partic(){
-	 var valido;
+	 var valido=false;
 	 var c=document.partForm.idCat.value;
 	 if(c!=null){
 	 valido=true;
 	 }else{
 	 M.toast({html: 'Debes seleccionar una categoría.'});
-	 valido=false;
 	 }
 	 return valido;
 	 }
@@ -48,14 +45,13 @@ function validarN_Partic() {
 	
 	 return true;
 	 }else{
-		 M.toast({html: 'El formato introducido en los campo de texto no es el correcto.'});
 	 return false;
 	 }
 	 
  }
 // ///////////////////////
 function validarN_ParticU()	{
-					var valido;
+					var valido=false;
 					 var n=document.partFormu.nombre.value;
 				   		var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
 				  		 var t=patt.test(n);
@@ -63,21 +59,23 @@ function validarN_ParticU()	{
 							valido=true;
 						}else{
 							 M.toast({html: 'El formato del nombre no es correcto.'});
-							valido=false;
+							 document.partFormu.nombre.style.color='red';
+							
 						}
 				   return valido;
 				   }
 
 					function validarA_ParticU(){
-						var valido;
+						var valido=false;
 					        var pa=/^[A-Z]{1}[a-zA-ZñÑçÇáéíóú ]{2,40}$/;
 					        var a=document.partFormu.apellido.value;
 					        var t=pa.test(a);
 									if(t){
 										valido=true;
 									}else{
-										alert("El formato de los apellidos no es correcto.");
-										valido=false;
+										M.toast({'El formato de los apellidos no es correcto'});
+										document.partFormu.apellido.style.color='red';
+									
 									}
 
 					       return valido;
@@ -88,7 +86,6 @@ function validarN_ParticU()	{
 									if(validarN_ParticU() && validarA_ParticU()){
 												return true;
 									}else{
-										alert("El formato introducido en los campo de texto no es el correcto.");
 										return false;
 									}
 								}
@@ -97,52 +94,52 @@ function validarN_ParticU()	{
 							
 								
 								function validarN_User(){
-									var valido;
+									var valido=false;
 										 var n=document.userform.nombre.value;
 									    var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
 									    var t=patt.test(n);
 											if(t){
 												valido=true;
 											}else{
-												alert("El formato del nombre no es correcto.");
-												valido=false;
-											}
+												M.toast({'El formato del nombre no es correcto.'});
+												document.userform.nombre.style.color='red';
+																							}
 									    return valido;
 									    }
 									
 									function validarE_User(){
-										var valido;
-										        var pa=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-										        var e=document.userform.email.value;
-										        var t=pa.test(e);
-														if(t){
-															valido=true;
+										var valido=false;
+										   var pa=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+										   var e=document.userform.email.value;
+										   var t=pa.test(e);
+												if(t){
+													valido=true;
 														}else{
-															alert("El formato del correo electrónico no es correcto.");
-															valido=false;
-														}
+															document.userform.email.style.color='red';
+															M.toast({'El formato del correo electrónico no es correcto.'});
+														}														
 									
 										       return valido;
 										      }
 									
-									function validarN_Usuario()	{
-										var valido;
+									function validarPwd_Usuario()	{
+										var valido=false;
 										 var n=document.userform.pass.value;
-									    var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ0-9 ][4,30}$");
+										 var patt=/^(?=.\d)(?=.[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/;
 									    var t=patt.test(n);
 											if(t){
 												valido=true;
 											}else{
-												alert("El mín de caracteres para la contraseña son 4 y el máximo 30.");
-												valido=false;
+												M.toast({'El mínimo de carácteres para la contraseña son 8 y el máximo 20. Además es obligatorio el uso de mayúsculas y números.'});
+												document.userform.pass.style.color='red';
+											
 											}
 									    return valido;
 									    }
 										    function vfU(){
-												if(validarN_User() && validarE_User() && validarN_Usuario()){
+												if(validarN_User() && validarE_User() && validarPwd_Usuario()){
 															return true;
 												}else{
-													alert("El formato introducido en los campo de texto no es el correcto.");
 													return false;
 												}
 											}
@@ -150,29 +147,30 @@ function validarN_ParticU()	{
 										  // //////////////////////////////
 
 											function validarNameupdate_User(){
-												var valido;
+												var valido=false;
 													 var n=document.userform.nombre.value;
 												    var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
 												    var t=patt.test(n);
 														if(t){
 															valido=true;
 														}else{
-															alert("El formato del nombre no es correcto.");
-															valido=false;
-														}
+															M.toast({'El formato del nombre no es correcto.'});
+															document.userform.nombre.style.color='red';
+																		}
 												    return valido;
 												    }
 												
 												function validarEmailupdate_User(){
-													var valido;
+													var valido=false;
 													        var pa=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 													        var e=document.userform.email.value;
 													        var t=pa.test(e);
 																	if(t){
 																		valido=true;
 																	}else{
-																		alert("El formato del correo electrónico no es correcto.");
-																		valido=false;
+																		M.toast({'El formato del correo electrónico no es correcto.'});
+																		document.userform.nombre.email.color='red';
+																	
 																	}
 												
 													       return valido;
@@ -183,9 +181,20 @@ function validarN_ParticU()	{
 															if(validarN_User() && validarE_User()){
 																		return true;
 															}else{
-																alert("El formato introducido en los campo de texto no es el correcto.");
 																return false;
 															}
 														}				    
 							         // ///////////////////////////////////////////////
-													    
+									function validarUpdate_Pwd(){
+										 var n=document.passForm.passwordNu.value;
+									    var patt=/^(?=.\d)(?=.[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/;
+									   var t=patt.test(n);
+											if(t){
+											    return true;
+											}else{
+									     		M.toast({'El mínimo de carácteres para la contraseña son 8 y el máximo 20. Además es obligatorio el uso de mayúsculas y números.'});
+												document.passForm.passwordNu.style.color='red';
+																return false;
+																}
+														
+														    }    
