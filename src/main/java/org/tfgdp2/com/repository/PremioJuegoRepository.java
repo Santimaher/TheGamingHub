77b3be7f93be6b1e9927c7e30fc404e1f640bc49
santi <santi@PORTATILSANTI>
2020-05-26 17:15:15 +0200

    Danger

--- src/main/java/org/tfgdp2/com/repository/PremioJuegoRepository.java
+++ src/main/java/org/tfgdp2/com/repository/PremioJuegoRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tfgdp2.com.domain.Foro;
 import org.tfgdp2.com.domain.Premio_Juego;
+import org.tfgdp2.com.domain.Premio_Participante;
 
 @Repository
 public interface PremioJuegoRepository extends JpaRepository<Premio_Juego, Long> {
+	public List<Premio_Juego> findByGalaId(Long idGala);
 }
