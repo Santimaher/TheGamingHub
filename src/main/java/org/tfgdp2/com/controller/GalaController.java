afe030a22df717e9d47a8f35b052490d4b4eaa7d
IvanPanber <62348504+IvanPanber@users.noreply.github.com>
2020-06-04 16:21:49 +0200

    chiodcsdpjcod

--- src/main/java/org/tfgdp2/com/controller/GalaController.java
+++ src/main/java/org/tfgdp2/com/controller/GalaController.java
@@ -1,6 +1,5 @@
 package org.tfgdp2.com.controller;
 
-
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
@@ -35,40 +34,44 @@ import org.tfgdp2.com.repository.PremioJuegoRepository;
 import org.tfgdp2.com.repository.PremioParticipanteRepository;
 
 @Controller
-@RequestMapping(value="/gala")
+@RequestMapping(value = "/gala")
 public class GalaController {
 
-	@Autowired GalaRepository repoGala;
-	@Autowired PremioJuegoRepository repoPremioJ;
-	@Autowired PremioParticipanteRepository repoPremioP;
-	@Autowired NominacionJuegoRepository repoNJuego;
-	@Autowired NominacionParticipanteRepository repoNParticipante;
+	@Autowired
+	GalaRepository repoGala;
+	@Autowired
+	PremioJuegoRepository repoPremioJ;
+	@Autowired
+	PremioParticipanteRepository repoPremioP;
+	@Autowired
+	NominacionJuegoRepository repoNJuego;
+	@Autowired
+	NominacionParticipanteRepository repoNParticipante;
+
 	@GetMapping("r")
 	public String read(ModelMap m) {
 		m.put("galas", repoGala.findAll());
 		m.put("view", "gala/r");
 		return "_t/frame";
 	}
-	
+
 	@GetMapping("c")
-	public String cGet(ModelMap m,HttpSession s) throws DangerException {
-		H.isRolOK("admin", s);		
+	public String cGet(ModelMap m, HttpSession s) throws DangerException {
+		H.isRolOK("admin", s);
 		m.put("view", "gala/C");
-		m.put("premiosJ",repoPremioJ.findAll());
-		m.put("premiosP",repoPremioP.findAll());
+		m.put("premiosJ", repoPremioJ.findAll());
+		m.put("premiosP", repoPremioP.findAll());
 		return "_t/frame";
 	}
+
 	@PostMapping("c")
-	public void cPost(ModelMap m,
-			@RequestParam("edicion") String edicion,
-			@RequestParam("inicio")
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
+	public void cPost(ModelMap m, @RequestParam("edicion") String edicion,
+			@RequestParam("inicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
 			@RequestParam(value = "premiosP[]") List<String> PremiosP,
 			@RequestParam(value = "premiosJ[]") List<String> PremiosJ,
-			@RequestParam("fin")
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin,
-			HttpSession s) throws DangerException, InfoException {
-		
+			@RequestParam("fin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin, HttpSession s)
+			throws DangerException, InfoException {
+
 		try {
 
 			Gala g = new Gala();
@@ -77,43 +80,45 @@ public class GalaController {
 			g.setFin(fin);
 			g.setActivo(false);
 			repoGala.save(g);
-			 for (String P : PremiosP) {
-			 Premio_Participante ppN=new Premio_Participante(P+"("+g.getFin().getMonth()+" "+g.getFin().getYear()+")");
-			 ppN.setTiene(g);
-			 repoPremioP.save(ppN);
-			 g.getPremiosP().add(ppN);  }
-			  
-			 for (String J : PremiosJ) {
-			 Premio_Juego pjN=new Premio_Juego(J+"("+g.getFin().getMonth()+" "+g.getFin().getYear()+")");
-			 pjN.setTiene(g);
-			 repoPremioJ.save(pjN);
-			 g.getPremiosJ().add(pjN);  }
-			 
+			for (String P : PremiosP) {
+				Premio_Participante ppN = new Premio_Participante(
+						P + "(" + g.getFin().getMonth() + " " + g.getFin().getYear() + ")");
+				ppN.setTiene(g);
+				repoPremioP.save(ppN);
+				g.getPremiosP().add(ppN);
+			}
+
+			for (String J : PremiosJ) {
+				Premio_Juego pjN = new Premio_Juego(J + "(" + g.getFin().getMonth() + " " + g.getFin().getYear() + ")");
+				pjN.setTiene(g);
+				repoPremioJ.save(pjN);
+				g.getPremiosJ().add(pjN);
+			}
+
 			repoGala.save(g);
-			
+
 		} catch (Exception e) {
-			PRG.error("Gala " + edicion + " duplicada"+e.getMessage(), "gala/c");
+			PRG.error("Gala " + edicion + " duplicada" + e.getMessage(), "gala/c");
 		}
 		PRG.info("Gala " + edicion + " creada correctamente", "gala/r");
 	}
+
 	@GetMapping("observacion")
-	public String observacion(ModelMap m,@RequestParam("id") Long id, HttpSession s) throws DangerException {
+	public String observacion(ModelMap m, @RequestParam("id") Long id, HttpSession s) throws DangerException {
 		H.isRolOK("admin", s);
 		m.put("gala", repoGala.getOne(id));
 		m.put("view", "gala/observacion");
 		return "_t/frame";
 	}
-	
+
 	@PostMapping("observacion")
-	public void anadirObservacion(ModelMap m,
-			@RequestParam("observacion") String observacion,
-			@RequestParam("id") Long id,
-			HttpSession s) throws DangerException, InfoException {		
+	public void anadirObservacion(ModelMap m, @RequestParam("observacion") String observacion,
+			@RequestParam("id") Long id, HttpSession s) throws DangerException, InfoException {
 		try {
 			Gala g = repoGala.getOne(id);
 			g.setObservaciones(observacion);
 			repoGala.save(g);
-			
+
 		} catch (Exception e) {
 			PRG.error("Fallo en añadir observacion", "gala/r");
 		}
@@ -121,26 +126,25 @@ public class GalaController {
 	}
 
 	@PostMapping("activar")
-	public void activar(@RequestParam("id") Long id,HttpSession s) throws DangerException, InfoException {	
-		
+	public void activar(@RequestParam("id") Long id, HttpSession s) throws DangerException, InfoException {
+
 		try {
 			H.isRolOK("admin", s);
-			Gala g = repoGala.getOne(id);			
-			if(g.getActivo()) {
+			Gala g = repoGala.getOne(id);
+			if (g.getActivo()) {
 				g.setActivo(false);
-			}
-			else {
-				g.setActivo(true);				
-				
+			} else {
+				g.setActivo(true);
+
 			}
 			repoGala.save(g);
-			
+
 		} catch (Exception e) {
 			PRG.error("Fallo en activar/desactivar gala", "gala/r");
 		}
 		PRG.info("Estado cambiado", "gala/r");
 	}
-	
+
 	@GetMapping("u")
 	public String uGet(@RequestParam("id") Long idGala, ModelMap m, HttpSession s) throws DangerException {
 		H.isRolOK("admin", s);
@@ -148,37 +152,33 @@ public class GalaController {
 		m.put("view", "gala/U");
 		return "_t/frame";
 	}
-	
+
 	@PostMapping("u")
-	public void uPost(ModelMap m,
-			@RequestParam("id") Long idGala,
-			@RequestParam("edicion") String edicion,
-			@RequestParam("inicio")
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
-			@RequestParam("fin")
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin,
-			HttpSession s) throws DangerException, InfoException {
-		
+	public void uPost(ModelMap m, @RequestParam("id") Long idGala, @RequestParam("edicion") String edicion,
+			@RequestParam("inicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
+			@RequestParam("fin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin, HttpSession s)
+			throws DangerException, InfoException {
+
 		try {
 			Gala g = repoGala.getOne(idGala);
 			g.setEdicion(edicion);
 			g.setInicio(inicio);
 			g.setFin(fin);
-			
+
 			repoGala.save(g);
-			
+
 		} catch (Exception e) {
 			PRG.error("Gala " + edicion + " duplicada", "gala/r");
 		}
 		PRG.info("Gala " + edicion + " actualizada correctamente", "gala/r");
 	}
-	
+
 	@PostMapping("d")
-	public void dPost(@RequestParam("id") Long id, ModelMap m, HttpSession s) throws DangerException, InfoException {		
+	public void dPost(@RequestParam("id") Long id, ModelMap m, HttpSession s) throws DangerException, InfoException {
 		try {
 			H.isRolOK("admin", s);
 			repoGala.delete(repoGala.getOne(id));
-			
+
 		} catch (Exception e) {
 			PRG.error("Error al borrar la Gala", "gala/r");
 		}
@@ -186,48 +186,34 @@ public class GalaController {
 		PRG.info("Gala borrada correctamente", "gala/r");
 
 	}
+
 	@GetMapping("GA")
-	public String GalaActual(ModelMap m, HttpSession s) 
-	{
-		List<Gala>galas=repoGala.findByOrderByEdicionDesc();
-		m.put("fecha",galas.get(0).getFin());
-		m.put("estado",galas.get(0).getActivo());
-		m.put("fechaActual",LocalDate.now());
-		m.put("fechaDePremio",galas.get(0).getFin().plusDays(5));
-		m.put("JuegoDeMayorImpacto",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Juego de Mayor Impacto", galas.get(0).getId()));
-		m.put("JuegoDelAnio",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Juego del Año", galas.get(0).getId()));
-		m.put("MejorBandaSonora",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Banda Sonora", galas.get(0).getId()));
-		m.put("MejorEventoDeEsport",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Evento de eSports", galas.get(0).getId()));
-		m.put("MejorJuegoComoServicio",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego como Servicio", galas.get(0).getId()));
-		m.put("MejorJuegoDeAccion",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego de Accion", galas.get(0).getId()));
-		m.put("MejorJuegoDeCarreras",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego de Deportes/Carreras", galas.get(0).getId()));
-		m.put("MejorJuegoDeEsports",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego de eSports", galas.get(0).getId()));
-		m.put("MejorJuegoDeEstrategia",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego de Estrategia", galas.get(0).getId()));
-		m.put("MejorJuegoDeLucha",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego de Lucha ", galas.get(0).getId()));
-        m.put("MejorJuegoDeRol",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego de rol" , galas.get(0).getId()));
-		m.put("MejorJuegoFamiliar",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego Familiar", galas.get(0).getId()));
-		m.put("MejorJuegoIndie",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego Indie", galas.get(0).getId()));
-		m.put("MejorJuegoMultijugador",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego Multijugador", galas.get(0).getId()));
-		m.put("MejorMoviles",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego para Moviles", galas.get(0).getId()));
-		m.put("MejorJuegoVR",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Juego VR", galas.get(0).getId()));
-		m.put("MejorNarrativa",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Narrativa", galas.get(0).getId()));
-		m.put("MejorComunidad",repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor Soporte a la Comunidad", galas.get(0).getId()));
+	public String GalaActual(ModelMap m, HttpSession s) {
+
+		Gala gala = repoGala.findTopByOrderByEdicionDesc();
+		List<Premio_Juego> pjs = (List<Premio_Juego>) gala.getPremiosJ();
+		List<Premio_Participante> pps = (List<Premio_Participante>) gala.getPremiosP();
 		
-		m.put("MejorActor",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor actor", galas.get(0).getId()));
-		m.put("MejorActorDeDoblaje",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor actor de doblaje", galas.get(0).getId()));
-		m.put("MejorAnfitrionEsports",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor anfitrion de eSports", galas.get(0).getId()));
-		m.put("MejorCreadorDeContenido",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor creador de contenido", galas.get(0).getId()));
-		m.put("MejorDesarrolladora",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor desarrolladora", galas.get(0).getId()));
-		m.put("MejorDireccion",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor direccion", galas.get(0).getId()));
-//		m.put("MejorDireccionArtistica",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor dirección Artistica", galas.get(0).getId()));
-		m.put("MejorEntrenador",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor entrenador de eSports", galas.get(0).getId()));
-		m.put("MejorEquipo",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor equipo de eSports", galas.get(0).getId()));
-		m.put("MejorGuionista",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor guionista", galas.get(0).getId()));
-		m.put("MejorJugador",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor jugador de eSports", galas.get(0).getId()));
-		m.put("MejorProduccion",repoNParticipante.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc("Mejor produccion audiovisual", galas.get(0).getId()));
+		for (Premio_Participante premio_Participante : pps) {
+			m.put(premio_Participante.getNombrePremio().replace(" ", ""),
+					repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc(
+							premio_Participante.getNombrePremio(), gala.getId()));
+
+		}
+		for (Premio_Juego premio_Juego : pjs) {
+			m.put(premio_Juego.getNombrePremio().replace(" ", ""),
+					repoNJuego.getTopByPremioNombrePremioAndPremioTieneIdOrderByCantidadVotosDesc(
+							premio_Juego.getNombrePremio(), gala.getId()));
+		}
+        m.put("pjs", pjs);
+        m.put("pps", pps);
+		m.put("fecha", gala.getFin());
+		m.put("estado", gala.getActivo());
+		m.put("fechaActual", LocalDate.now());
+		m.put("fechaDePremio", gala.getFin().plusDays(5));
 		
-		
-		m.put("view","gala/galaActiva");
+
+		m.put("view", "gala/galaActiva");
 		return "_t/frame";
 	}
 }
