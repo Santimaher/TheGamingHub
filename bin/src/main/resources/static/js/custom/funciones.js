a2a7fd269ca4304d10ae5ff1cc7a091ed76b36c9
IvanPanber <62348504+IvanPanber@users.noreply.github.com>
2020-06-15 17:26:56 +0200

    ccdcdcdcdcdc

---
+++ bin/src/main/resources/static/js/custom/funciones.js
@@ -0,0 +1,86 @@
+function buscar() {
+	clear();
+	var aguja = document.getElementById('busca').value;
+	var res = getJSON();
+	var arr = res[0].plataformas;
+	for (let consola of arr) {
+		if(aguja == consola.familia){
+			var boton = document.createElement("button");
+			boton.id = "btn-"+consola.id;
+			var texto=document.createTextNode(consola.nombre);
+			boton.append(texto);
+			boton.type="button";
+			boton.setAttribute("onclick",'anadir("'+consola.nombre+'",'+consola.id+')');
+			boton.setAttribute("class",'btn waves-effect waves-light purple');
+			document.getElementById('exp').append(boton);
+			
+		
+			}		
+	}
+	}
+function clear(){
+	document.getElementById('exp').innerHTML = "";
+}		
+	
+
+
+
+function getJSON() {
+	var resp = [];
+	$.ajax({
+		url : 'https://thegaminghub.herokuapp.com/js/custom/plataformas.txt',
+		type : 'GET',
+		dataType : 'json',
+		async : false,
+		success : function(data) {
+			resp.push(data);
+		},
+		error : function(req, err) {
+			console.log(err);
+		}
+	})
+	return resp;
+}
+
+function anadir(nombre,id){
+	if(esta(nombre)){
+		alert("La consola: "+nombre+", ya esta seleccionada");
+	}
+	
+	else{
+		var divo = document.createElement("div");	
+		divo.setAttribute("class","chip purple white-text");
+		var contTexto = document.createElement("span");
+		contTexto.setAttribute("name","consolas");
+		var texto2 = document.createTextNode(nombre);
+		contTexto.append(texto2);
+		divo.append(contTexto);
+		var oculto = document.createElement("input");
+		oculto.type="hidden";
+		oculto.name="idPlataforma[]";
+		oculto.value=id;
+		divo.append(oculto);
+		var cerrar = document.createElement("i");
+		cerrar.classList.add("close");
+		cerrar.classList.add("material-icons");	 
+		var texto3 = document.createTextNode("close");
+		cerrar.append(texto3);
+		divo.append(cerrar);
+		document.getElementById('selec').append(divo);
+	}
+	
+}
+function esta(nombre){
+	var check = false;
+	var seleccionados = document.getElementsByName("consolas");
+	for (let unidad of seleccionados) {
+		if (unidad.innerText == nombre) {
+			check=true;
+		}
+	}
+	return check
+}
+
+
+	
+			
---
+++ bin/src/main/resources/static/js/custom/validaciones.js
@@ -0,0 +1,211 @@
+
+function validarN_Partic() {
+	 var valido=false;
+	 var n=document.partForm.nombre.value;
+	 var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
+	 var t=patt.test(n);
+	 if(t){
+	 valido=true;
+	 }else{
+		 
+	 M.toast({html: 'El formato del nombre no es correcto.'}); 
+	 document.partForm.nombre.style.color='red';
+	 
+	 }
+	 return valido;
+	 }
+
+	 function validarA_Partic(){
+	 var valido=false;
+	 var pa=/^[A-Z]{1}[a-zA-ZñÑçÇáéíóú ]{2,40}$/;
+	 var a=document.partForm.apellido.value;
+	 var t=pa.test(a);
+	 if(t){
+	 valido=true;
+	 }else{
+		M.toast({html: 'El formato de los apellidos no es correcto.'});
+	 document.partForm.apellido.style.color='red';
+	 }
+
+	 return valido;
+	 }
+
+	 function validarC_Partic(){
+	 var valido=false;
+	 var c=document.partForm.idCat.value;
+	 if(c!=null){
+	 valido=true;
+	 }else{
+	 M.toast({html: 'Debes seleccionar una categoría.'});
+	 }
+	 return valido;
+	 }
+
+	 function vfP(){
+	 if(validarN_Partic() && validarA_Partic()&& validarC_Partic()){
+	
+	 return true;
+	 }else{
+	 return false;
+	 }
+	 
+ }
+// ///////////////////////
+function validarN_ParticU()	{
+					var valido=false;
+					 var n=document.partFormu.nombre.value;
+				   		var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
+				  		 var t=patt.test(n);
+						if(t){
+							valido=true;
+						}else{
+							 M.toast({html: 'El formato del nombre no es correcto.'});
+							 document.partFormu.nombre.style.color='red';
+							
+						}
+				   return valido;
+				   }
+
+					function validarA_ParticU(){
+						var valido=false;
+					        var pa=/^[A-Z]{1}[a-zA-ZñÑçÇáéíóú ]{2,40}$/;
+					        var a=document.partFormu.apellido.value;
+					        var t=pa.test(a);
+									if(t){
+										valido=true;
+									}else{
+										M.toast({html: 'El formato de los apellidos no es correcto'});
+										document.partFormu.apellido.style.color='red';
+									
+									}
+
+					       return valido;
+					      }
+
+
+								function vfPu(){
+									if(validarN_ParticU() && validarA_ParticU()){
+												return true;
+									}else{
+										return false;
+									}
+								}
+								
+// /////////// usuario c uUser
+							
+								
+								function validarN_User(){
+									var valido=false;
+										 var n=document.userform.nombre.value;
+									    var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
+									    var t=patt.test(n);
+											if(t){
+												valido=true;
+											}else{
+												M.toast({ html: 'El formato del nombre no es correcto.'});
+												document.userform.nombre.style.color='red';
+																							}
+									    return valido;
+									    }
+									
+									function validarE_User(){
+										var valido=false;
+										   var pa=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+										   var e=document.userform.email.value;
+										   var t=pa.test(e);
+												if(t){
+													valido=true;
+														}else{
+															document.userform.email.style.color='red';
+															M.toast({html:'El formato del correo electrónico no es correcto.'});
+														}														
+									
+										       return valido;
+										      }
+									
+									function validarPwd_Usuario()	{
+										var valido=false;
+										 var pass=document.userform.pass.value;
+										 var patt1=/^[a-zA-Z0-9]{8,20}$/;
+									      var patt2=/[A-Z]{1,20}/
+									      var patt3=/[0-9]{1,20}/
+									      console.log(patt1.test(pass)&&patt2.test(pass)&&patt3.test(pass));
+										
+									    var t=(patt1.test(pass)&&patt2.test(pass)&&patt3.test(pass));
+											if(t){
+												valido=true;
+											}else{
+												M.toast({html:'El mínimo de carácteres para la contraseña son 8 y el máximo 20. Además es obligatorio el uso de mayúsculas y números.'});
+												document.userform.pass.style.color='red';
+											
+											}
+									    return valido;
+									    }
+										    function vfU(){
+												if(validarN_User() && validarE_User() && validarPwd_Usuario()){
+															return true;
+												}else{
+													return false;
+												}
+											}
+											
+										  // //////////////////////////////
+
+											function validarNameupdate_User(){
+												var valido=false;
+													 var n=document.userform.nombre.value;
+												    var patt = new RegExp("^[a-zA-ZáéíóúñÑçÇ ]{2,30}$");
+												    var t=patt.test(n);
+														if(t){
+															valido=true;
+														}else{
+															M.toast({html:'El formato del nombre no es correcto.'});
+															document.userform.nombre.style.color='red';
+																		}
+												    return valido;
+												    }
+												
+												function validarEmailupdate_User(){
+													var valido=false;
+													        var pa=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+													        var e=document.userform.email.value;
+													        var t=pa.test(e);
+																	if(t){
+																		valido=true;
+																	}else{
+																		M.toast({html:'El formato del correo electrónico no es correcto.'});
+																		document.userform.nombre.email.color='red';
+																	
+																	}
+												
+													       return valido;
+													      }
+												
+									
+													    function vformupdateUser(){
+															if(validarN_User() && validarE_User()){
+																		return true;
+															}else{
+																return false;
+															}
+														}				    
+							         // ///////////////////////////////////////////////
+									function validarUpdate_Pwd(){
+										 var pass=document.passForm.passwordNu.value;
+										
+										 var patt1=/^[a-zA-Z0-9]{8,20}$/;
+									      var patt2=/[A-Z]{1,20}/
+									      var patt3=/[0-9]{1,20}/
+									      console.log(patt1.test(pass)&&patt2.test(pass)&&patt3.test(pass));
+									    var t=(patt1.test(pass)&&patt2.test(pass)&&patt3.test(pass));
+									    
+											if(t){
+											    return true;
+											}else{
+									     		M.toast({html:'El mínimo de carácteres para la contraseña son 8 y el máximo 20. Además es obligatorio el uso de mayúsculas y números.'});
+												document.passForm.passwordNu.style.color='red';
+																return false;
+																}
+														
+														    }
+																		
